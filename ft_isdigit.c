a0f194140f5d3e9c6c40dbb31cf331610b717e4f
Freek Bes <fbes@student.codam.nl>
2020-11-01 21:52:31 +0100

    every function now has its own file

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,46 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_ascii.c                                         :+:    :+:            */
+/*   ft_isdigit.c                                       :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2020/10/26 20:32:48 by fbes          #+#    #+#                 */
-/*   Updated: 2020/11/01 21:16:31 by fbes          ########   odam.nl         */
+/*   Created: 2020/11/01 21:49:10 by fbes          #+#    #+#                 */
+/*   Updated: 2020/11/01 21:49:11 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
-
-int		ft_isalpha(int c)
-{
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-		return (1);
-	return (0);
-}
 
 int		ft_isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
-		return (1);
-	return (0);
-}
-
-int		ft_isalnum(int c)
-{
-	if (ft_isalpha(c) || ft_isdigit(c))
-		return (1);
-	return (0);
-}
-
-int		ft_isascii(int c)
-{
-	if (c >= 0 && c <= 127)
-		return (1);
-	return (0);
-}
-
-int		ft_isprint(int c)
-{
-	if (c >= 32 && c <= 126)
 		return (1);
 	return (0);
 }
--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -1,30 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_put_fd.c                                        :+:    :+:            */
+/*   ft_putendl_fd.c                                    :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2020/10/28 13:28:30 by fbes          #+#    #+#                 */
-/*   Updated: 2020/11/01 17:03:00 by fbes          ########   odam.nl         */
+/*   Created: 2020/11/01 21:51:22 by fbes          #+#    #+#                 */
+/*   Updated: 2020/11/01 21:51:38 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
-
-void	ft_putstr_fd(char *s, int fd)
-{
-	while (*s != '\0')
-	{
-		ft_putchar_fd(*s, fd);
-		s++;
-	}
-}
 
 void	ft_putendl_fd(char *s, int fd)
 {
