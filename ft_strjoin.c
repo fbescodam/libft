a9cfaf2f540c51886265ced28fcb8e75a7529e75
Freek Bes <fbes@student.codam.nl>
2020-11-01 17:12:54 +0100

    improved strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/27 15:00:32 by fbes          #+#    #+#                 */
-/*   Updated: 2020/10/27 15:08:59 by fbes          ########   odam.nl         */
+/*   Updated: 2020/11/01 17:12:17 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	dest = (char *)malloc(s1_len + s2_len + 1);
 	if (dest)
 	{
-		ptr = dest;
-		ft_memcpy(ptr, s1, s1_len);
-		ptr += s1_len;
-		ft_memcpy(ptr, s2, s2_len);
-		ptr += s2_len;
-		*ptr = '\0';
+		ft_memcpy(dest, s1, s1_len);
+		ft_memcpy(dest + s1_len, s2, s2_len + 1);
 	}
 	return (dest);
 }
