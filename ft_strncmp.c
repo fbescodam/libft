59b628e6b38544b13117972c8c03da49998b0db5
Freek Bes <freekbladnl@gmail.com>
2020-11-03 16:23:17 +0100

    fixed char overflow issue in strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/17 13:37:33 by fbes          #+#    #+#                 */
-/*   Updated: 2020/10/28 16:20:29 by fbes          ########   odam.nl         */
+/*   Updated: 2020/11/03 16:22:17 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,5 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 		return (0);
 	while (s1[i] == s2[i] && s1[i] != '\0' && i < n - 1)
 		i++;
-	return (s1[i] - s2[i]);
+	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 }
