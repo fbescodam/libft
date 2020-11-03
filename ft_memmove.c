9fb21c42f08592e52e3b669b6a019edd3a7d19a7
Freek Bes <freekbladnl@gmail.com>
2020-11-03 16:11:43 +0100

    fixed a bug with overlapping memory

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/26 17:14:02 by fbes          #+#    #+#                 */
-/*   Updated: 2020/11/01 16:59:09 by fbes          ########   odam.nl         */
+/*   Updated: 2020/11/03 16:10:54 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,21 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 
 	src_cpy = src;
 	dest_cpy = dest;
-	if (src + n > dest)
-	{
-		while (n > 0)
-		{
-			n--;
-			dest_cpy[n] = src_cpy[n];
-		}
-	}
-	else
+	if (dest < src)
 	{
 		while (n > 0)
 		{
 			*dest_cpy = *src_cpy;
 			dest_cpy++;
 			src_cpy++;
+			n--;
+		}
+	}
+	else
+	{
+		while (n > 0)
+		{
+			dest_cpy[n - 1] = src_cpy[n - 1];
 			n--;
 		}
 	}
