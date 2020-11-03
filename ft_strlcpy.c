71e9c4ab7542012e768c57453d0b2c47a3c6dd5a
Freek Bes <freekbladnl@gmail.com>
2020-11-03 16:17:05 +0100

    fixed null-termination on strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/15 18:46:07 by fbes          #+#    #+#                 */
-/*   Updated: 2020/10/28 13:16:23 by fbes          ########   odam.nl         */
+/*   Updated: 2020/11/03 16:15:44 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,17 @@
 
 size_t		ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	int		last_char_reached;
 	size_t	i;
 
-	if (size > 0)
+	if (size != 0)
 	{
-		last_char_reached = 0;
 		i = 0;
-		while (i < size - 1)
+		while (i < size - 1 && src[i] != '\0')
 		{
-			if (last_char_reached > 0 || src[i] == '\0')
-			{
-				last_char_reached = 1;
-				dest[i] = '\0';
-			}
-			else
-				dest[i] = src[i];
+			dest[i] = src[i];
 			i++;
 		}
-		dest[size - 1] = '\0';
+		dest[i] = '\0';
 	}
-	else
-		dest[0] = '\0';
 	return (ft_strlen(src));
 }
