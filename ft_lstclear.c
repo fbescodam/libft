fa25d5b89232dd51a289c5c49ac65309276706a8
Freek Bes <fbes@student.codam.nl>
2020-11-07 16:27:01 +0100

    check if **lst == NULL

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/03 17:32:17 by fbes          #+#    #+#                 */
-/*   Updated: 2020/11/03 19:42:05 by fbes          ########   odam.nl         */
+/*   Updated: 2020/11/07 16:24:16 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	t_list	*temp1;
 	t_list	*temp2;
 
-	temp1 = *lst;
-	while (temp1)
+	if (lst)
 	{
-		temp2 = temp1->next;
-		ft_lstdelone(temp1, del);
-		temp1 = temp2;
+		temp1 = *lst;
+		while (temp1)
+		{
+			temp2 = temp1->next;
+			ft_lstdelone(temp1, del);
+			temp1 = temp2;
+		}
+		*lst = NULL;
 	}
-	*lst = NULL;
 }
